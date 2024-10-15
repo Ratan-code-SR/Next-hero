ddea2cae3c5f9023b1a9c9ba4e0afccaaba2ae90
ratan chakma <ratanchakma68@gmail.com>
2024-10-15 22:05:02 +0600

    navbar updated

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
+    const navItem = [
+        {
+            title:"Home",
+            path:"/home"
+        },
+        {
+            title:"About",
+            path:"/about"
+        },
+        {
+            title:"Services",
+            path:"/services"
+        },
+        {
+            title:"Blog",
+            path:"/blog"
+        },
+        {
+            title:"Contact",
+            path:"/contact"
+        },
+    ]
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -22,36 +46,29 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                           {navItem.map((item) => (
+                                <li key={item.path}>
+                                    <Link className="hover:text-primary" href={item.path}>{item.title}</Link>
+                                </li>
+                            ))}
+                            
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link href={"/"}>
+                    <Image alt="logo" src={"/assets/logo.svg"} height={60} width={100}/>
+                    </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    {navItem.map((item) => (
+                                <li key={item.path}>
+                                    <Link className="hover:text-primary" href={item.path}>{item.title}</Link>
+                                </li>
+                            ))}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                <button className="btn btn-outline btn-warning">Appointment</button>
                 </div>
             </div>
         </div>

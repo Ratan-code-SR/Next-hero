a69b936f9deeb09bb0fc74a004086dd71c78c148
ratan chakma <ratanchakma68@gmail.com>
2024-10-29 22:19:26 +0600

    file updated

--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -12,7 +12,12 @@ const About = () => {
             <h3 className='text-3xl text-orange-500 font-bold'>About Us</h3>
             <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
             <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
-            <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+            <button className="btn btn-primary">Get More Info</button>
+            <button className="btn btn-primary">Get More Info</button>
+            <button className="btn btn-primary">Get More Info</button>
+            <button className="btn btn-primary">Get More Info</button>
+            <button className="btn btn-primary">Get More Info</button>
+            <button className="btn btn-primary">Get More Info</button>
             <button className="btn btn-primary">Get More Info</button>
         </div>
     </div>
